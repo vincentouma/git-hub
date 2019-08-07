d9608becd6f663e58df401fa9ddf1a5e4f83258d
vincentouma <vinceoumah@gmail.com>
2019-08-07 09:05:47 +0300

    add  config apploadset

--- app/__init__.py
+++ app/__init__.py
@@ -34,6 +34,7 @@ def create_app(config_name):
     # configure UploadSet
     configure_uploads(app,photos)
 
+
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
