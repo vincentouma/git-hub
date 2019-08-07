da3ae14a4c83c5b3bb036716c899767540b23ca7
vincentouma <vinceoumah@gmail.com>
2019-08-07 09:11:42 +0300

    update app

--- manage.py
+++ manage.py
@@ -3,10 +3,7 @@ from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 from app.models import User, Comments, Pitches, PitchListing
 
-# app = create_app('test')
 
-# Creating app instance
-# app = create_app('production')
 app = create_app('development')
 
 manager = Manager(app)
