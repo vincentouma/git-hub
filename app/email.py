fba87da0ad088ebe5e16ff9cb356bdc30bda56f1
vincentouma <vinceoumah@gmail.com>
2019-08-07 09:07:41 +0300

    add send email funtion

--- app/email.py
+++ app/email.py
@@ -2,7 +2,6 @@ from flask_mail import Message
 from flask import render_template
 from . import mail
 
-
 def mail_message(subject,template,to,**kwargs):
     sender_email= 'amiratiffany@gmail.com'
     email = Message(subject, sender=sender_email, recipients=[to])
